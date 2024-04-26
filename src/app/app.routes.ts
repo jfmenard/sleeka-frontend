6a159babd484c55755241b25ac6a366580b09e60
Jean-Francois Menard <jeanfrancoismenard@gmail.com>
2024-04-26 07:59:35 -0400

    Created new post component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from "@angular/router";
+import { NewPostComponent } from "./modules/home/new-post/new-post.component";
 import { PostComponent } from "./modules/home/post/post.component";
 import { PostsListComponent } from "./modules/home/posts-list/posts-list.component";
 
@@ -6,4 +7,5 @@ export const routes: Routes = [
     { path: '', component: PostsListComponent },
     { path: 's/:subsleekUrlName/p/:postId', component: PostComponent },
     { path: 's/:subsleekUrlName', component: PostsListComponent },
+    { path: '/p/new', component: NewPostComponent },
 ];
